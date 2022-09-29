f19fd9c0a848fc75a6bc093ff84ed6e5c9cb8467
Sanket Banerjee <sanketbanerjee.2004@gmail.com>
2022-09-30 02:03:27 +0530

    Second Commit

--- database.js
+++ database.js
@@ -1,9 +1,12 @@
-var rollV, nameV, genderV, addressV;
+var rollV, nameV,telephoneV,emailV, genderV,semV, addressV;
 
 function readFom() {
   rollV = document.getElementById("roll").value;
   nameV = document.getElementById("name").value;
+  telephoneV = document.getElementById("telephone").value;
+  emailV = document.getElementById("email").value;
   genderV = document.getElementById("gender").value;
+  semV = document.getElementById("semester").value;
   addressV = document.getElementById("address").value;
   console.log(rollV, nameV, addressV, genderV);
 }
@@ -36,7 +39,10 @@ document.getElementById("read").onclick = function () {
     .on("value", function (snap) {
       document.getElementById("roll").value = snap.val().rollNo;
       document.getElementById("name").value = snap.val().name;
+      document.getElementById("telephone").value = snap.val().telephone;
+      document.getElementById("email").value = snap.val().email;
       document.getElementById("gender").value = snap.val().gender;
+      document.getElementById("semester").value = snap.val().semester;
       document.getElementById("address").value = snap.val().address;
     });
 };
@@ -48,15 +54,21 @@ document.getElementById("update").onclick = function () {
     .database()
     .ref("student/" + rollV)
     .update({
-      //   rollNo: rollV,
-      name: nameV,
+      //rollNo: rollV,
+      //name: nameV,
+      telephone: telephoneV,
+      email:emailV,
       gender: genderV,
+      semester: semV,
       address: addressV,
     });
   alert("Data Update");
   document.getElementById("roll").value = "";
   document.getElementById("name").value = "";
+  document.getElementById("telephone").value = "";
+  document.getElementById("email").value = "";
   document.getElementById("gender").value = "";
+  document.getElementById("semester").value = "";
   document.getElementById("address").value = "";
 };
 document.getElementById("delete").onclick = function () {
@@ -69,7 +81,10 @@ document.getElementById("delete").onclick = function () {
   alert("Data Deleted");
   document.getElementById("roll").value = "";
   document.getElementById("name").value = "";
+  document.getElementById("telephone").value = "";
+  document.getElementById("email").value = "";
   document.getElementById("gender").value = "";
+  document.getElementById("semester").value = "";
   document.getElementById("address").value = "";
 };
 
